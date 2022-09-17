be39de69834fc4d7e4ff53cdfb6544b33139105e
bogus <qazxwd44@hanmail.net>
2022-09-18 01:07:51 +0900

    node bird 1일차

--- app.js
+++ app.js
@@ -34,3 +34,21 @@ app.use(
     },
   })
 );
+
+app.use("/", pageRouter);
+app.use(req, res, (next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.stauts = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.stauts || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
