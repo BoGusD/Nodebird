155518a54790aee4359f39c434d6f20c5147b03a
bogus <qazxwd44@hanmail.net>
2022-09-20 23:28:20 +0900

    노드버드 login 구현

--- app.js
+++ app.js
@@ -3,31 +3,30 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const path = require("path");
 const session = require("express-session");
-const nunjucks = require("nunjunks");
+const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const pageRouter = require("/routes/page");
-const { json } = require("sequelize");
+const pageRouter = require("./routes/page");
 
-const app = exrpess();
-app.set("port", process.env.PORT || 8001);
+const app = express();
+app.set("port", process.env.PORT || 5500);
 app.set("view engine", "html");
-nunjunks.configure("views", {
+nunjucks.configure("view", {
   express: app,
   watch: true,
 });
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(porcess.env.COOKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    secret: Process.env.COOKIE_SECRET,
+    secret: process.env.COOKIE_SECRET,
     cookie: {
       httpOnly: true,
       secure: false,
@@ -36,16 +35,17 @@ app.use(
 );
 
 app.use("/", pageRouter);
-app.use(req, res, (next) => {
+
+app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.stauts = 404;
+  error.status = 404;
   next(error);
 });
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
-  res.status(err.stauts || 500);
+  res.status(err.status || 500);
   res.render("error");
 });
 
